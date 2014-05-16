8277e89b469e6274aab19d1074103ff2900cb17c
tao <popacai@gmail.com>
2014-05-16 00:36:43 -0700

    pipe

--- MTCP.py
+++ MTCP.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from threading import Thread
+
 class MTCP():
-    def __init__(self, pipe):
+    def __init__(self, output_pipe):
+        #All the message we have received, we will write to output_pipe
         pass
 
-    def connect(self, addrs):
-        #addrs[] list
+    def new_connect(self, addr, input_pipe):
+        #This will create connection with addr
+        #It will auto fetch data from input_pipe
         pass
 
-    def send_all(self, data):
-        pass
-
-    def recv(self, data):
-        pass
+if __name__ == "__main__":
+    pass
 
 
--- PIPE.py
+++ PIPE.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import os
 class PIPE():
     def __init__(self):
-        pass
+        self.r, self.w = os.pipe()
+        self.writer = os.fdopen(self.w, "w", 0)
+        self.reader = os.fdopen(self.r, "r", 0)
 
     def read(self):
-        pass
-
+        data = self.reader.readline()[:-1]
+        data = data.replace("\x01", "\n")
+        return data
     def write(self, data):
-        pass
+        data = data.replace("\n", "\x01")
+        self.writer.write(data + "\n")
+
 
 if __name__ == "__main__":
     p = PIPE()
-    p.write("123")
-    print `p.read()
+    for i in range(10):
+        p.write(str(i))
+    while True:
+        print p.read()+"*"

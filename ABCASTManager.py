e313caf2c27091160a5693216d8d78395f6615e3
wizard1990 <wizard19900509@gmail.com>
2014-05-16 00:02:04 -0700

    init ABCASTManager

---
+++ ABCASTManager.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import PIPE
+
+
+class ABCASTManager(object):
+    """Manager for sending ABCAST"""
+    def __init__(self, addr):
+        super(ABCASTManager, self).__init__()
+        self.inputPipe = PIPE()
+        self.outputPipe = PIPE()
+        self.addr = addr
+        self.clientList = []
+
+    def read(self):
+        pass
+
+    def write(self, message):
+        pass
+
+    def quit(self):
+        pass
+
+if __name__ == '__main__':
+    pass

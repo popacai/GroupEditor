7e83f2a7db216559ed96073403a408c462283ecf
tao <popacai@gmail.com>
2014-05-15 23:47:25 -0700

    MTCP, PIPE

---
+++ MTCP.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class MTCP():
+    def __init__(self, pipe):
+        pass
+
+    def connect(self, addrs):
+        #addrs[] list
+        pass
+
+    def send_all(self, data):
+        pass
+
+    def recv(self, data):
+        pass
+
+
